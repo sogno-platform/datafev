83d313596a6711abf7c15cb5feb337f8c65940cb
Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>
2021-11-05 14:41:42 +0100

    Added optimal_allocation_min_violation function.
    Signed-off-by: Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>

--- management_functions/cluster_based_cs_station/positioncontrol.py
+++ management_functions/cluster_based_cs_station/positioncontrol.py
@@ -1,4 +1,4 @@
-from management_algorithms.multicluster.unbalance_analytical import minimize_cluster_unabalance
+from management_algorithms.multicluster.allocation_analytical import minimize_inter_cluster_unabalance,minimize_cluster_capacity_violation
 
 def optimal_allocation_min_unbalance(cs, ts, time_delta, opt_horizon, ev_schedule, cc_schedules):
     """
@@ -9,8 +9,24 @@ def optimal_allocation_min_unbalance(cs, ts, time_delta, opt_horizon, ev_schedul
     candidate_clusters = cluster_occupation_actual[cluster_occupation_actual < cs.cu_numbers].index
 
     if len(candidate_clusters) > 0:
-        optimal_cluster = minimize_cluster_unabalance(cc_schedules, ev_schedule, candidate_clusters,cs.cc_capacities)
+        optimal_cluster = minimize_inter_cluster_unabalance(cc_schedules, ev_schedule, candidate_clusters,cs.cc_capacities)
     else:
         optimal_cluster = None
 
+    return optimal_cluster
+
+def optimal_allocation_min_violation(cs, ts, time_delta, opt_horizon, ev_schedule, cc_schedules):
+    """
+    This function identifies the candidate clusters and calls the unbalance minimization function before allocation
+    """
+
+    cluster_occupation_profile = cs.get_cluster_occupations(ts, time_delta, opt_horizon)
+    cluster_occupation_actual = cluster_occupation_profile.iloc[0]  # Current occupation
+    candidate_clusters = cluster_occupation_actual[cluster_occupation_actual < cs.cu_numbers].index
+
+    if len(candidate_clusters) > 0:
+        optimal_cluster = minimize_cluster_capacity_violation(cc_schedules, ev_schedule, candidate_clusters, cs.cc_capacities)
+    else:
+        optimal_cluster = None
+    
     return optimal_cluster

4058b5d5cbe40375cc20248c469f90c7c281cc0c
Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>
2021-11-22 15:49:58 +0100

    TODOs for Aytug
    Signed-off-by: Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>

--- management_functions/cs_clustered/positioncontrol.py
+++ management_functions/cs_clustered/positioncontrol.py
@@ -1,4 +1,5 @@
 from management_algorithms.multicluster.allocation_analytical import minimize_inter_cluster_unabalance,minimize_cluster_capacity_violation
+from management_algorithms.multicluster.allocation_optimization import optimal_costdif_cluster
 
 def optimal_allocation_min_unbalance(cs, ts, time_delta, opt_horizon, ev_schedule, cc_schedules):
     """
@@ -31,10 +32,20 @@ def optimal_allocation_min_violation(cs, ts, time_delta, opt_horizon, ev_schedul
     
     return optimal_cluster
 
-def optimal_allocation_cluster_cost_diff(cs,ts,time_delta,opt_horizon,ev_schedule):
-    
-    cluster_cost_series=dynamicpricing_analytical()
-    
-    optimal_costdif_cluster()
-    
-    return optimal_cluster,p_ref,s_ref
+def optimal_allocation_cluster_cost_diff(cs,ev,ts,time_delta,opt_horizon,solver):
+    """
+    This function jointly optimizes the cluster allocation and schedule of an incoming EV for given cluster differentiating price signals 
+    """
+    #TODO: 
+    #1) Necessary input parsing to run dynamic pricing_analytical())
+    #
+    #
+    #cluster_cost_series={}
+    #for c in cs.clusters.keys():
+    #    cluster_cost_series[c]=dynamicpricing_analytical()
+    #2) Necessary input parsing to call optimal_costdif_cluster()
+    #
+    #
+    #p_ref,soc_ref,optimal_cluster=optimal_costdif_cluster(solver,arrts,leavets,time_delta,p_ch,p_ds,ecap,inisoc,tarsoc,minsoc,maxsoc,costcoeffs)
+    #3) return p_ref,soc_ref,optimal_cluster
+    return 0

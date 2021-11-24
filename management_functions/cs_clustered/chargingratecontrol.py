9dd5adeb1f06699975f98f5546b5ea0213129a38
Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>
2021-11-24 11:44:40 +0100

    Management function to run optimal_intervention_v2g algorithm added.
    Signed-off-by: Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>

--- management_functions/cs_clustered/chargingratecontrol.py
+++ management_functions/cs_clustered/chargingratecontrol.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from management_algorithms.multicluster.intervention import short_term_rescheduling
+from management_algorithms.multicluster.intervention_bidirectional import short_term_rescheduling_bidirectional
 from management_algorithms.cluster.decentralized_intervention import decentralized_short_term_rescheduling
 from management_functions.cu.min_max import calc_p_max_ch
 
@@ -22,12 +23,9 @@ def optimal_intervention_v1g(cs, ts, t_delta, horizon, solver):
         initial_soc = {}
         desired_soc = {}
         location = {}
-
         for cc_id, cc in cs.clusters.items():
-
             for cu_id, cu in cc.cu.items():
                 connected_car = cu.connected_car
-
                 if connected_car != None:
                     ev_id = connected_car.vehicle_id
                     location[ev_id] = (cc_id, cu_id)
@@ -153,6 +151,74 @@ def simple_intervention_v1g(cs, ts, t_delta):
                     cu.supply(ts, t_delta, set_points[cc_id][cu_id])
                 else:
                     cu.idle(ts, t_delta)
+    else:
+        for cc_id in cs.clusters.keys():
+            for cu_id in cs.clusters[cc_id].cu.keys():
+                cu = cs.clusters[cc_id].cu[cu_id]
+                cu.idle(ts, t_delta)
+                
+                
+def optimal_intervention_v2g(cs, ts, t_delta, horizon, solver, powlimits):
+    opt_horizon_t_steps = pd.date_range(start=ts, end=ts + horizon, freq=t_delta)
+    occ = cs.get_cluster_occupations(ts, t_delta, t_delta)
+    if occ.iloc[0].sum() > 0:
+
+        parkdata = {}
+        parkdata['clusters']    = [*sorted(cs.clusters.keys())]
+        parkdata['opt_horizon'] = list(range(len(opt_horizon_t_steps)))
+        parkdata['con_horizon'] = [0]
+        parkdata['opt_step']    = t_delta
+
+        connections={}
+        connections['P_EV_pos_max']={}
+        connections['P_EV_neg_max']={}
+        connections['charge_eff']={}
+        connections['discharge_eff']={}
+        connections['battery_cap']={}
+        connections['target_soc']={}
+        connections['departure_time']={}
+        connections['initial_soc']={}
+        connections['location']={}
+        for cc_id, cc in cs.clusters.items():
+            for cu_id, cu in cc.cu.items():
+                connected_car = cu.connected_car
+                
+                if connected_car != None:
+                    ev_id = connected_car.vehicle_id
+                    connections['location'] [ev_id]      = (cc_id, cu_id)
+                    connections['battery_cap'][ev_id]    = connected_car.bCapacity
+                    connections['departure_time'][ev_id] = (connected_car.estimated_leave-ts)/t_delta
+                    connections['initial_soc'][ev_id]    = connected_car.soc[ts]
+                    sch_inst = cu.active_schedule_instance
+                    cu_sch = cu.schedule_soc[sch_inst]  
+                    cu_sch = cu_sch.reindex(opt_horizon_t_steps)
+                    cu_sch = cu_sch.fillna(method="ffill")
+                    connections['target_soc'][ev_id]     = cu_sch[ts + horizon]
+                    connections['charge_eff'][ev_id]     = cu.eff       
+                    connections['discharge_eff'][ev_id]  = cu.eff  
+                                 
+                    if cu.ctype=='ac1':
+                        p_ev_pos_max=min(connected_car.p_max_ac_ph1,cu.P_max_ch)
+                        p_ev_neg_max=min(connected_car.p_max_ac_ph1,cu.P_max_ds) #TODO: Differntiate ev.p_max_ac_ph1_ch/ds
+                    if cu.ctype=='ac3':
+                        p_ev_pos_max=min(connected_car.p_max_ac_ph3,cu.P_max_ch)
+                        p_ev_neg_max=min(connected_car.p_max_ac_ph3,cu.P_max_ds)
+                    if cu.ctype=='dc':
+                        p_ev_pos_max=min(connected_car.p_max_dc,cu.P_max_ch)
+                        p_ev_neg_max=min(connected_car.p_max_dc,cu.P_max_ds)
+                    
+                    connections['P_EV_pos_max'][ev_id]   =p_ev_pos_max*cu.eff
+                    connections['P_EV_neg_max'][ev_id]   =p_ev_neg_max 
+                    
+        set_points,soc_ref=short_term_rescheduling_bidirectional(parkdata,powlimits,connections,solver)
+        for cc_id in cs.clusters.keys():
+            for cu_id in cs.clusters[cc_id].cu.keys():
+                cu = cs.clusters[cc_id].cu[cu_id]
+                if cu.connected_car != None:
+                    cu.supply(ts, t_delta, set_points[cc_id, cu_id][0])
+                else:
+                    cu.idle(ts, t_delta)
+
     else:
         for cc_id in cs.clusters.keys():
             for cu_id in cs.clusters[cc_id].cu.keys():

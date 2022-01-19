dc92f70c3d2759acd2f2e25448a05b895451ad0e
Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>
2022-01-19 17:15:17 +0100

    Result analysis functions added
    Signed-off-by: Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>

--- utils/result_analyze.py
+++ utils/result_analyze.py
@@ -8,53 +8,48 @@ def print_results(cs, ev_fleet, period, energy_price, xlfile):
     
     with pd.ExcelWriter(xlfile) as writer:
         pow_to_cu_dict = {}
-        occup_cu_dict  = {}
+        occ_of_cu_dict = {}
         pow_to_cc_df= pd.DataFrame()
         occup_cc_df = pd.DataFrame()
-        csu_cc_dict = {} 
-        dsh_cc_dict = {}
-        v2x_cc_dict = {}
+        
+        g2v_cc_r = {}           #Simulation result grid-to-vehicle 
+        g2v_cc__ = {}           #Difference between schedule vs simulation result grid-to-vehicle 
+        v2x_cc_r = {}           #Simulation result vehicle-to-x
+        v2x_cc__ = {}           #Difference between schedule vs simulation result vehicle-to-x     
 
-        for cc_id in sorted(cs.clusters.keys()):
+        for cc_id,cc in sorted(cs.clusters.items()):
 
-            pow_to_cu_dict[cc_id] = pd.DataFrame()
-            occup_cu_dict[cc_id] = pd.DataFrame()
-            cc = cs.clusters[cc_id]
             cc_ds=cc.cc_dataset
             cc_ds.to_excel(writer, sheet_name='Cluster_' + cc_id)
-               
-            for cu_id in sorted(cc.cu.keys()):
-                cu = cc.cu[cu_id]
-                pow_to_cu_dict[cc_id][cu_id] = cu.consumed_power[t_start:t_end]
-                occup_cu_dict[cc_id][cu_id]  = cu.occupation_record(t_start,t_end,t_delta)
-            pow_to_cc_df[cc_id] = pow_to_cu_dict[cc_id].sum(axis=1)
-            occup_cc_df[cc_id] = occup_cu_dict[cc_id].sum(axis=1)
-            csu_cc_dict[cc_id] = cc_ds['Charged Energy [kWh]'].sum() 
-            dsh_cc_dict[cc_id] = ((cc_ds['Feasible Target SOC']-cc_ds['Leave SOC'])*cc_ds['Car Battery Capacity']/3600).sum()
             
-            cc_v2g=0
-            for entry_id in cc_ds.index:
-                ev_id  =cc_ds.loc[entry_id,'Car ID']
-                ev     =ev_fleet[ev_id]
-                ev_v2x =(pd.Series(ev.v2x)* t_delta.seconds / 3600).sum()
-                cc_v2g+=ev_v2x
-            v2x_cc_dict[cc_id] = cc_v2g
-                
+            pow_to_cu_dict[cc_id] = cc.import_profile(t_start,t_end,t_delta)
+            pow_to_cc_df[cc_id]   = pow_to_cu_dict[cc_id].sum(axis=1)
+            occ_of_cu_dict[cc_id] = cc.occupation_profile(t_start,t_end,t_delta) 
+            occup_cc_df[cc_id]    = occ_of_cu_dict[cc_id].sum(axis=1)         
+            
+            g2v_cc_r[cc_id] = cc_ds['Net G2V [kWh]'].sum()
+            unfulfilled_g2v = cc_ds['Scheduled G2V [kWh]']-cc_ds['Net G2V [kWh]']
+            g2v_cc__[cc_id] = (unfulfilled_g2v[unfulfilled_g2v>0]).sum()
+            
+            v2x_cc_r[cc_id] = cc_ds['Total V2X [kWh]'].sum() 
+            excessive_v2x   = cc_ds['Total V2X [kWh]']-cc_ds['Scheduled V2X [kWh]']
+            v2x_cc__[cc_id] = (excessive_v2x[excessive_v2x>0]).sum()
+                              
         power_cu_df = pd.concat(pow_to_cu_dict, axis=1)
-        occup_cu_df = pd.concat(occup_cu_dict, axis=1)
-        #pow_to_cc_df['Total'] = pow_to_cc_df.sum(axis=1).copy()
-
-        occup_cu_df.to_excel(writer, sheet_name='Occupation_CU')
         power_cu_df.to_excel(writer, sheet_name='Power_CU')
+        
+        occup_cu_df = pd.concat(occ_of_cu_dict, axis=1)
+        occup_cu_df.to_excel(writer, sheet_name='Occupation_CU')
+        
         occup_cc_df.to_excel(writer, sheet_name='Occupation_CC')
         pow_to_cc_df.to_excel(writer, sheet_name='Power_CC')
         
-
-        energy_summary = pd.DataFrame(columns=['Import (kWh)','Cost of Import (EUR)', 'Cumulative Supply (kWh)','Unfulfilled Demand (kWh)', 'V2X Utilization (kWh)'])
+        energy_summary = pd.DataFrame(columns=['Import (kWh)','Cost of Import (EUR)', 'Cumulative Supply (kWh)','Cumulative V2X (kWh)','Unfulfilled Demand (kWh)', 'Excessive V2X (kWh)'])
         energy_summary['Import (kWh)']            = (pow_to_cc_df * t_delta.seconds / 3600).sum()
-        energy_summary['Cost of Import (EUR)']    = (pow_to_cc_df.mul(energy_price/1000)).sum()
-        energy_summary['Cumulative Supply (kWh)'] = pd.Series(csu_cc_dict)
-        energy_summary['Unfulfilled Demand (kWh)']= pd.Series(dsh_cc_dict)
-        energy_summary['V2X Utilization (kWh)']   = pd.Series(v2x_cc_dict )
+        energy_summary['Cost of Import (EUR)']    = ((pow_to_cc_df* t_delta.seconds / 3600).multiply(energy_price/1000,axis='index')).sum()
+        energy_summary['Cumulative Supply (kWh)'] = pd.Series(g2v_cc_r)
+        energy_summary['Cumulative V2X (kWh)']    = pd.Series(v2x_cc_r)
+        energy_summary['Unfulfilled Demand (kWh)']= pd.Series(g2v_cc__)
+        energy_summary['Excessive V2X (kWh)']     = pd.Series(v2x_cc__)
         energy_summary.loc['Total',:]= energy_summary.sum()
         energy_summary.to_excel(writer, sheet_name='Cost')

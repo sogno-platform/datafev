9ed1fbc3fc59ea0652155a8a01f6b3c0201ce142
Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>
2022-10-12 16:18:52 +0200

    Renaming of the data handling classes
    Signed-off-by: Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>

--- datahandling/Fleet.py
+++ datahandling/Fleet.py
@@ -5,7 +5,8 @@ Created on Tue Mar  8 14:50:05 2022
 @author: egu
 """
 
-from datahandling.EV import ElectricVehicle
+from datahandling.vehicle import ElectricVehicle
+import pandas as pd
 
 class EVFleet(object):
     
@@ -16,47 +17,62 @@ class EVFleet(object):
         self.reserving_at=dict([(t,[]) for t in sim_horizon])
         self.incoming_at =dict([(t,[]) for t in sim_horizon])
         self.outgoing_at =dict([(t,[]) for t in sim_horizon])
-        
-        self.define_behavior(behavior)
-        
-        
-    def define_behavior(self,behavior):
-    
-        for _,i in behavior.iterrows(): 
+        self.outgoing_at[None]=[]
+
+        ##################################################################################################
+        #Define behavior
+        for _,i in behavior.iterrows():
     
             #Initialization of an EV object
             evID     =i['ev_id']
             bcap     =i['Battery Capacity (kWh)']
-            p_max_ac1=i['p_max_ac1']
-            p_max_ac3=i['p_max_ac3']
-            p_max_dc =i['p_max_dc']
-            ev       =ElectricVehicle(evID,bcap,p_max_ac1,p_max_ac3,p_max_dc)      
+            p_max_ch =i['p_max_ch']
+            p_max_ds =i['p_max_ds']
+            ev       =ElectricVehicle(evID,bcap,p_max_ch,p_max_ds)
             
             #Assigning the scenario parameters
             ev.t_res               =i['Reservation Time']
             ev.t_arr_est           =i['Estimated Arrival Time']
             ev.t_dep_est           =i['Estimated Departure Time']        
             ev.soc_arr_est         =i['Estimated Arrival SOC']       
-            ev.t_crt               =i['Critical Time']
-            ev.soc_tar_at_t_crt    =i['Target SOC @ Critical Time']
             ev.soc_tar_at_t_dep_est=i['Target SOC @ Estimated Departure Time']
-            ev.v2x_allow           =i['V2X Allowance (kWh)']*3600
+            ev.v2g_allow           =i['V2G Allowance (kWh)']*3600
             ev.t_arr_real          =i['Real Arrival Time']
             ev.soc_arr_real        =i['Real Arrival SOC']
             ev.t_dep_real          =i['Real Departure Time']
+            ev.cluster_target      =i['Target Cluster']
             ev.soc[ev.t_arr_real ] =ev.soc_arr_real 
                      
             self.objects[evID]=ev
             self.reserving_at[ev.t_res].append(ev)
             self.incoming_at[ev.t_arr_real].append(ev)
-            self.outgoing_at[ev.t_dep_real].append(ev)
-        
-    def incoming_vehicles_at(self,ts): 
+
+            if pd.isna(ev.t_dep_real):
+                self.outgoing_at[None].append(ev)
+            else:
+                self.outgoing_at[ev.t_dep_real].append(ev)
+        ##################################################################################################
+
+        ##################################################################################################
+        #Calculate statistics
+        self.presence_distribution = {}
+        for t in sim_horizon:
+            self.presence_distribution[t] = len(behavior[(behavior['Real Arrival Time'] <= t) &
+                                                         (behavior['Real Departure Time'] > t)])
+        ##################################################################################################
+
+    def enter_power_soc_table(self,table):
+        for ev_id,ev in self.objects.items():
+            ev.pow_soc_table=table.loc[ev_id].copy()
+
+    def reserving_vehicles_at(self,ts):
+        return self.reserving_at[ts]
+
+    def incoming_vehicles_at(self,ts):
         return self.incoming_at[ts]
     
     def outgoing_vehicles_at(self,ts):
         return self.outgoing_at[ts]
-    
         
         
         
--- datahandling/vehicle.py
+++ datahandling/vehicle.py
@@ -5,7 +5,7 @@
 
 class ElectricVehicle(object):
     
-    def __init__(self,carID,bCapacity,p_max_ac_ph1=7.7,p_max_ac_ph3=22,p_max_dc=350,minSoC=0.2,maxSoC=1.0):
+    def __init__(self,carID,bCapacity,p_max_ch=50,p_max_ds=50,minSoC=0.0,maxSoC=1.0,pow_soc_table=None):
         """
         This method initializes the car objects.
         """
@@ -13,16 +13,16 @@ class ElectricVehicle(object):
         self.vehicle_id   = carID
         self.bCapacity    = bCapacity*3600     #kWh to kWs
         
-        self.p_max_ac_ph1 = p_max_ac_ph1 
-        self.p_max_ac_ph3 = p_max_ac_ph3
-        self.p_max_dc     = p_max_dc
+        self.p_max_ch       = p_max_ch
+        self.p_max_ds       = p_max_ds
+        self.pow_soc_table = pow_soc_table
 
         self.minSoC = minSoC
         self.maxSoC = maxSoC
         self.soc    = {}
-        self.v2x    = {}
-        self.x2v    = {}
-                              
+        self.v2g    = {}
+        self.g2v    = {}
+
     def charge(self,ts,tdelta,p_in):
         """
         ts    : datetime
@@ -34,8 +34,8 @@ class ElectricVehicle(object):
         p_in<0 discharging
         """  
         self.soc[ts+tdelta]=self.soc[ts]+p_in*tdelta.seconds/self.bCapacity
-        self.v2x[ts]       =-p_in if p_in<0 else 0
-        self.x2v[ts]       = p_in if p_in>0 else 0
+        self.v2g[ts]       =-p_in if p_in<0 else 0
+        self.g2v[ts]       = p_in if p_in>0 else 0
         
 #    def request_available_charger_list(self,server,tdelta,advance_reservation=False):
 #        

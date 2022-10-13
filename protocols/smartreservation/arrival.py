7d9ae2f0ed46bfd952cb8112a2058d5885dd4815
Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>
2022-10-13 08:29:14 +0200

    Package name changed.
    Signed-off-by: Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>

--- protocols/smartreservation/arrival.py
+++ protocols/smartreservation/arrival.py
@@ -9,6 +9,13 @@ import numpy as np
 import pandas as pd
 
 def arrival_protocol(ts,tdelta,fleet):
+    """
+    This protocol is executed upon arrival of EVs that have smart reservations
+    :param ts:
+    :param tdelta:
+    :param fleet:
+    :return:
+    """
 
     incoming_vehicles = fleet.incoming_vehicles_at(ts)
 
@@ -28,7 +35,6 @@ def arrival_protocol(ts,tdelta,fleet):
 
             else:
                 # The reserved charger is occupied by another EV
-
                 old_reservation_id  =ev.reservation_id
                 old_reservation     =reserved_cluster.re_dataset.loc[old_reservation_id]
 
@@ -42,15 +48,18 @@ def arrival_protocol(ts,tdelta,fleet):
                                                 (available_cus['eff']==reserved_charger.eff)]
 
                     if len(identical_cus)>0:
-                        #There are identical available chargers
+                        # There are available chargers with same characteristics as the previously reserved one
+                        # Reserve an identicle charger
                         new_reserved_charger_id       = identical_cus.idxmin()
 
                     else:
                         # There is no available charger identical to the reserved one
-                        new_reserved_charger_id = available_cus['max p_ch'].idxmin()
+                        # Reserve the charger with maximum power capability
+                        new_reserved_charger_id = available_cus['max p_ch'].idxmax()
 
                     new_reserved_charger = reserved_cluster.chargers[new_reserved_charger_id]
 
+                    # The conditions of old reservations will be aimed in the new reservation
                     old_reservation_time          = old_reservation['Reserved At']
                     old_reservation_scheduled_g2v = old_reservation['Scheduled G2V']
                     old_reservation_scheduled_v2g = old_reservation['Scheduled V2G']
@@ -65,13 +74,17 @@ def arrival_protocol(ts,tdelta,fleet):
 
                     new_reserved_charger.set_schedule(ts, old_reservation_p_schedule, old_reservation_s_schedule)
 
+                    # Old reservation will be removed
                     reserved_charger.unreserve(ts,old_reservation_id)
-
-
 
                     ev.admitted=True
 
                 else:
                     # There is no available charger
                     ev.admitted=False
+
+        else:
+            # The EV has no reservation will be rejected
+            # TODO: Add a reservation protocl
+            ev.admitted=False
 

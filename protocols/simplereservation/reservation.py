fab111055c150b6036b129f5b062907f46799b27
Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>
2022-10-14 14:22:05 +0200

    Simple reservation protocols
    Signed-off-by: Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>

--- protocols/simplereservation/reservation.py
+++ protocols/simplereservation/reservation.py
@@ -10,13 +10,15 @@ import numpy as np
 
 def reservation_protocol(ts, tdelta, system, fleet, traffic_forecast):
     """
-    This protocol is executed to reserve a charger for the EVs approaching clusters.
-    The simple reservations specify which charger the approaching EVs must connect to in the target cluster.
+    This protocol is executed to reserve chargers for the EVs approaching a multi-cluster system.
+
+    The smart reservations specifies the cluster and charger the approaching EVs must connect to.
 
     :param ts:                Current time                                                      datetime
     :param tdelta:            Resolution of scheduling                                          timedelta
     :param system:            Multi-cluster system object                                       datahandling.multiclust
     :param fleet:             EV fleet object                                                   datahandling.fleet
+    :param traffic_forecast:  Traffic forecast data                                             dict of dict
 
     """
 
@@ -24,24 +26,38 @@ def reservation_protocol(ts, tdelta, system, fleet, traffic_forecast):
 
     for ev in reserving_vehicles:
 
-        # The EV communicates with the cluster operator
-        target_cluster_id=ev.cluster_target
-        target_cluster   =system.clusters[target_cluster_id]
+        ############################################################################
+        ############################################################################
+        #Step 1: Identify available chargers
+        available_chargers=system.query_availability(ev.t_arr_est,ev.t_dep_est,tdelta,traffic_forecast)
+        ############################################################################
+        ############################################################################
 
-        # Charger availability check
-        available_cus =target_cluster.query_availability(ts,ev.t_dep_est,tdelta)
-
-        if len(available_cus) > 0:
-
-            # There is available charger
-            selected_charger_id = np.random.choice(list(available_cus.index))
-            selected_charger = target_cluster.chargers[selected_charger_id]
-            ev.reserved = True
-
-            # Reserve the charger until estimated departure time
-            target_cluster.reserve(ts, ts, ev.t_dep_est, ev, selected_charger)
-
+        if len(available_chargers)==0:
+            ev.reserved=False
         else:
+            ############################################################################
+            ############################################################################
 
-            # There is no available charger for the considered period
-            ev.reseved = False
+            #Step 2: Apply a specific reservation management strategy
+            #In the simple resevation strategy, an available charger is selected randomly
+            selected_charger_id = np.random.choice(list(available_chargers.index))
+            selected_cluster_id = available_chargers.loc[selected_charger_id, 'Cluster']
+            selected_cluster    = system.clusters[selected_cluster_id]
+            selected_charger    = selected_cluster.chargers[selected_charger_id]
+            #End: Reservation management strategy
+
+            ############################################################################
+            ############################################################################
+
+            ############################################################################
+            ############################################################################
+            #Step 3: Reserve the selected charger for the EV and assign relevant reservation parameters
+            res_at   =ts
+            res_from =ev.t_arr_est+traffic_forecast['arr_del'][selected_cluster_id]
+            res_until=ev.t_dep_est+traffic_forecast['dep_del'][selected_cluster_id]
+
+            selected_cluster.reserve(res_at,res_from,res_until,ev,selected_charger)
+            ev.reserved = True
+            ############################################################################
+            ############################################################################

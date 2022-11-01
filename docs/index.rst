eff1517fa23edbb69a316f8ecce46a997c4abac9
Aytug Yavuzer <aytug.yavuzer@rwth-aachen.de>
2022-11-01 16:28:08 +0100

    New project structure
    Signed-off-by: Aytug Yavuzer <aytug.yavuzer@rwth-aachen.de>

---
+++ docs/index.rst
@@ -0,0 +1,20 @@
+.. datafev documentation master file, created by
+   sphinx-quickstart on Tue Nov  1 16:26:30 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to datafev's documentation!
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- src/tutorials/scenariogeneration/scenario_generation_independent_times.py
+++ src/tutorials/scenariogeneration/scenario_generation_independent_times.py
@@ -24,7 +24,7 @@ def main():
     
 
     arr_times_dict, dep_times_dict, arr_soc_dict, dep_soc_dict, ev_dict = \
-        ut.excel_to_sceneration_input_independent_times(file_path=r'C:\Users\aytugy\Desktop\workspace\datafev\src\datafev\tutorials\scenariogeneration\input_generator.xlsx')
+        ut.excel_to_sceneration_input_independent_times(file_path=r'C:\Users\aytugy\Desktop\workspace\datafev\src\tutorials\scenariogeneration\input_generator.xlsx')
         
     ev_df = sc.generate_fleet_data_independent_times(arr_soc_dict=arr_soc_dict, dep_soc_dict=dep_soc_dict, ev_dict=ev_dict,
                                   number_of_evs_per_day=5, startdate=dt.date(2021, 6, 1), enddate=dt.date(2021, 6, 3),
@@ -32,14 +32,14 @@ def main():
                                   dep_times_dict=dep_times_dict, dep_day_prob_distribution=dep_day_prob_distribution)
 
     
-    ut.visualize_statistical_time_generation(r'C:\Users\aytugy\Desktop\workspace\datafev\src\datafev\outputs', ev_df, timedelta_in_min=15)
+    ut.visualize_statistical_time_generation(r'C:\Users\aytugy\Desktop\workspace\datafev\src\outputs', ev_df, timedelta_in_min=15)
     
     # Unlocalize datetimes, as Excel does not support datetimes with timezones
     ev_df['ArrivalTime'] = ev_df['ArrivalTime'].dt.tz_localize(None)
     ev_df['DepartureTime'] = ev_df['DepartureTime'].dt.tz_localize(None)
-    ev_df.to_excel(r'C:\Users\aytugy\Desktop\workspace\datafev\src\datafev\outputs\output_generator_independent_times.xlsx')
+    ev_df.to_excel(r'C:\Users\aytugy\Desktop\workspace\datafev\src\outputs\output_generator_independent_times.xlsx')
     
-    ut.output_to_sim_input(ev_df, r'C:\Users\aytugy\Desktop\workspace\datafev\src\datafev\outputs\input_simulator_independent_times.xlsx')
+    ut.output_to_sim_input(ev_df, r'C:\Users\aytugy\Desktop\workspace\datafev\src\outputs\input_simulator_independent_times.xlsx')
     
 if __name__ == "__main__":
     main()

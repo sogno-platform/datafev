b18d3cb82074b4733768c30a5f9192004c20f5d5
Aytug Yavuzer <aytug.yavuzer@rwth-aachen.de>
2022-11-01 13:56:07 +0100

    Updated project structure
    Signed-off-by: Aytug Yavuzer <aytug.yavuzer@rwth-aachen.de>

--- src/datafev/tutorials/scenariogeneration/scenario_generation_dependent_times.py
+++ src/datafev/tutorials/scenariogeneration/scenario_generation_dependent_times.py
@@ -16,21 +16,21 @@ def main():
     """
     
     times_dict, arr_dep_times_dict, arr_soc_dict, dep_soc_dict, ev_dict = \
-        ut.excel_to_sceneration_input_dependent_times(file_path=r'C:\Users\aytugy\Desktop\workspace\charger-clusters\tutorials\scenariogeneration\input_generator.xlsx')
+        ut.excel_to_sceneration_input_dependent_times(file_path=r'C:\Users\aytugy\Desktop\workspace\datafev\src\datafev\tutorials\scenariogeneration\input_generator.xlsx')
 
     ev_df = sc.generate_fleet_data_dependent_times(arr_soc_dict=arr_soc_dict, dep_soc_dict=dep_soc_dict, ev_dict=ev_dict,
                                  number_of_evs=5, startdate=dt.date(2021, 6, 1), enddate=dt.date(2021, 6, 3),
                                  timedelta_in_min=15, diff_arr_dep_in_min=0, times_dict=times_dict,
                                  arr_dep_times_dict=arr_dep_times_dict)
     
-    ut.visualize_statistical_time_generation(r'C:\Users\aytugy\Desktop\workspace\charger-clusters\outputs', ev_df, timedelta_in_min=15)
+    ut.visualize_statistical_time_generation(r'C:\Users\aytugy\Desktop\workspace\datafev\src\datafev\outputs', ev_df, timedelta_in_min=15)
     
     # Unlocalize datetimes, as Excel does not support datetimes with timezones
     ev_df['ArrivalTime'] = ev_df['ArrivalTime'].dt.tz_localize(None)
     ev_df['DepartureTime'] = ev_df['DepartureTime'].dt.tz_localize(None)
-    ev_df.to_excel(r'C:\Users\aytugy\Desktop\workspace\charger-clusters\outputs\output_generator_dependent_times.xlsx')          
+    ev_df.to_excel(r'C:\Users\aytugy\Desktop\workspace\datafev\src\datafev\outputs\output_generator_dependent_times.xlsx')
     
-    ut.output_to_sim_input(ev_df, r'C:\Users\aytugy\Desktop\workspace\charger-clusters\outputs\input_simulator_dependent_times.xlsx')
+    ut.output_to_sim_input(ev_df, r'C:\Users\aytugy\Desktop\workspace\datafev\src\datafev\outputs\input_simulator_dependent_times.xlsx')
     
 if __name__ == "__main__":
     main()

f5fde971934a2dcae2fa6527ef5785db4bd72457
Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>
2022-11-07 15:44:59 +0100

    Tutorial header corrections
    Signed-off-by: Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>

--- src/tutorials/simulations/simulation_smart_reservations.py
+++ src/tutorials/simulations/simulation_smart_reservations.py
@@ -1,15 +1,36 @@
+# The datafev framework
+
+# Copyright (C) 2022,
+# Institute for Automation of Complex Power Systems (ACS),
+# E.ON Energy Research Center (E.ON ERC),
+# RWTH Aachen University
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 from datetime import datetime, timedelta
 from pyomo.environ import SolverFactory
 import matplotlib.pyplot as plt
 
-from src.datafev.datahandling.fleet import EVFleet
-from src.datafev.datahandling.cluster import ChargerCluster
-from src.datafev.datahandling.multicluster import MultiClusterSystem
+from datafev.datahandling.fleet import EVFleet
+from datafev.datahandling.cluster import ChargerCluster
+from datafev.datahandling.multicluster import MultiClusterSystem
 
-from src.datafev.protocols.smartreservation.reservation import *
-from src.datafev.protocols.smartreservation.arrival import *
-from src.datafev.protocols.departure import *
-from src.datafev.protocols.chargingcontrol.decentralized_milp import charging_protocol
+from datafev.protocols.smartreservation.reservation import *
+from datafev.protocols.smartreservation.arrival import *
+from datafev.protocols.departure import *
+from datafev.protocols.chargingcontrol.decentralized_milp import charging_protocol
 
 # Simulation parameters
 sim_start = datetime(2022, 1, 8, 7)
--- src/tutorials/simulations/simulation_without_reservations.py
+++ src/tutorials/simulations/simulation_without_reservations.py
@@ -1,17 +1,38 @@
+# The datafev framework
+
+# Copyright (C) 2022,
+# Institute for Automation of Complex Power Systems (ACS),
+# E.ON Energy Research Center (E.ON ERC),
+# RWTH Aachen University
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+# Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+# WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+# OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from pyomo.environ import SolverFactory
 
-from src.datafev.datahandling.fleet import EVFleet
-from src.datafev.datahandling.cluster import ChargerCluster
-from src.datafev.datahandling.multicluster import MultiClusterSystem
+from datafev.datahandling.fleet import EVFleet
+from datafev.datahandling.cluster import ChargerCluster
+from datafev.datahandling.multicluster import MultiClusterSystem
 
-from src.datafev.protocols.arrival import *
-from src.datafev.protocols.departure import *
-from src.datafev.protocols.chargingcontrol.decentralized_fcfs import (
+from datafev.protocols.arrival import *
+from datafev.protocols.departure import *
+from datafev.protocols.chargingcontrol.decentralized_fcfs import (
     charging_protocol as charging_protocol_fcs,
 )
-from src.datafev.protocols.chargingcontrol.decentralized_llf import (
+from datafev.protocols.chargingcontrol.decentralized_llf import (
     charging_protocol as charging_protocol_llf,
 )
 

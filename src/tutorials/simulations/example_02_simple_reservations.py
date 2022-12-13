9e33823e03610615fd16bcf1efcb4c0049a763cd
Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>
2022-12-13 15:50:08 +0100

    Lines fixed
    Signed-off-by: Erdem Gümrükcü <erdem.guemruekcue@rwth-aachen.de>

--- src/tutorials/simulations/example_02_simple_reservations.py
+++ src/tutorials/simulations/example_02_simple_reservations.py
@@ -175,7 +175,7 @@ def main():
     
     #Line charts to visualize cluster loading and occupation profiles
     fig1=system.visualize_cluster_loading(sim_start, sim_end, sim_step)   
-    fig2=system.visualize_cluster_loading(sim_start, sim_end, sim_step)   
+    fig2=system.visualize_cluster_occupation(sim_start, sim_end, sim_step)   
     plt.show()
 
     
